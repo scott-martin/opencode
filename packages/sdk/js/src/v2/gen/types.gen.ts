ac25f3c738ce6daf9c9bad7356e29720fddaee2e
GitHub Action <action@github.com>
2025-12-18 16:39:21 +0000

    chore: format code

--- packages/sdk/js/src/v2/gen/types.gen.ts
+++ packages/sdk/js/src/v2/gen/types.gen.ts
@@ -846,6 +846,14 @@ export type KeybindsConfig = {
    */
   session_timeline?: string
   /**
+   * Fork session from message
+   */
+  session_fork?: string
+  /**
+   * Rename session
+   */
+  session_rename?: string
+  /**
    * Share current session
    */
   session_share?: string
